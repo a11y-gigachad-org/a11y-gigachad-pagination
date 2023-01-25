7178289a16e04a61927e4c2f571276edf74fe49d
John James <john.usa.james@gmail.com>
2023-01-25 22:26:42 +0000

    feat(pagination): add page handlers

--- src/Pagination.tsx
+++ src/Pagination.tsx
@@ -1,17 +1,55 @@
 export type PaginationProps = {
-  children: React.ReactNode
-  title: string
+  totalCount: number
+  currentPage: number
+  pageSize: number
+  onPageChange: (page: number) => void
 }
 
 const Pagination = (props: PaginationProps) => {
-  const { children, title } = props
+  const { totalCount, currentPage, pageSize, onPageChange } = props
+
+  const onNext = () => {
+    if (currentPage !== Math.ceil(totalCount / pageSize)) {
+      onPageChange(currentPage + 1)
+    }
+  }
+
+  const onPrevious = () => {
+    if (currentPage !== 1) {
+      onPageChange(currentPage - 1)
+    }
+  }
+
+  const pageNumbers = []
+
+  for (let i = 1; i <= Math.ceil(totalCount / pageSize); i++) {
+    pageNumbers.push(i)
+  }
 
   return (
-    <section>
-      <h1>{title}</h1>
+    <nav>
+      <ol>
+        <li>
+          <button type="button" onClick={onPrevious}>
+            Prev
+          </button>
+        </li>
 
-      {children}
-    </section>
+        {pageNumbers.map((page) => (
+          <li key={page}>
+            <button type="button" onClick={() => onPageChange(page)}>
+              {page}
+            </button>
+          </li>
+        ))}
+
+        <li>
+          <button type="button" onClick={onNext}>
+            Next
+          </button>
+        </li>
+      </ol>
+    </nav>
   )
 }
 
