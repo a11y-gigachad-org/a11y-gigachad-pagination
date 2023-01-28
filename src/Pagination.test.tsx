6f0e6fa285a52986369c76d36620205abdaa84c0
John James <john.usa.james@gmail.com>
2023-01-28 13:39:15 +0000

    test(pagination): write test cases out

--- src/Pagination.test.tsx
+++ src/Pagination.test.tsx
@@ -2,19 +2,49 @@ import { render, screen } from "@testing-library/react"
 
 import { Pagination } from "."
 
-it("should render `title` and `children`", () => {
+const data = ["john", "doe", "jane"]
+
+test("should render controls", () => {
   const onPageChange = jest.fn()
 
-  const items = ["h"]
+  render(<Pagination currentPage={1} setCurrentPage={onPageChange} limit={2} total={data.length} />)
 
-  render(
-    <Pagination
-      currentPage={1}
-      onPageChange={onPageChange}
-      pageSize={3}
-      totalCount={items.length}
-    />
-  )
+  expect(screen.getByRole("button", { name: "go to the next page" })).toBeInTheDocument()
+  expect(screen.getByRole("button", { name: "go to the previous page" })).toBeInTheDocument()
+  expect(screen.getByRole("button", { name: "go to page 1" })).toBeInTheDocument()
+})
 
-  expect(screen.getByText("1")).toBeInTheDocument()
+test("should render initial labels", async () => {
+  const onPageChange = jest.fn()
+
+  render(<Pagination currentPage={1} setCurrentPage={onPageChange} limit={2} total={data.length} />)
+
+  expect(screen.getByText("Current page 1 out of 2")).toBeInTheDocument()
+})
+
+test("should render labels as per `currentPage`", async () => {
+  const onPageChange = jest.fn()
+
+  render(<Pagination currentPage={2} setCurrentPage={onPageChange} limit={2} total={data.length} />)
+
+  expect(screen.getByText("Current page 2 out of 2")).toBeInTheDocument()
+})
+
+test("should have a11y", async () => {
+  const onPageChange = jest.fn()
+
+  render(<Pagination currentPage={1} setCurrentPage={onPageChange} limit={2} total={data.length} />)
+
+  expect(screen.getByRole("button", { name: "go to the next page" })).toHaveAccessibleDescription()
+  expect(screen.getByRole("button", { name: "go to the next page" })).toHaveAccessibleName()
+
+  expect(
+    screen.getByRole("button", { name: "go to the previous page" })
+  ).toHaveAccessibleDescription()
+  expect(screen.getByRole("button", { name: "go to the previous page" })).toHaveAccessibleName()
+
+  expect(screen.getByRole("button", { name: "go to page 1" })).toHaveAccessibleDescription()
+  expect(screen.getByRole("button", { name: "go to page 1" })).toHaveAccessibleName()
+
+  expect(screen.getByText("Current page 1 out of 2")).toHaveAttribute("role", "status")
 })
